eeeff1ade0bb043605d0d0dff4007595343934ea
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 14:17:31 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -5,37 +5,37 @@ import Section2 from './Section2';
 import Section3 from './Section3';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/" id="form-link">
-                Section 1
-              </Link>
-            </li>
-            <li>
-              <Link to="/section2" id="form-ref-link">
-                Section 2
-              </Link>
-            </li>
-            <li>
-              <Link to="/section3" id="form-state-link">
-                Section 3
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes> 
-          <Route path="/" element={<Section1 />} /> 
-          <Route path="/section2" element={<Section2 />} /> 
-          <Route path="/section3" element={<Section3 />} /> 
-        </Routes> 
-      </div>
-    </Router>
-  );
-}
-
-export default App;
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/form" id="form-link">
+                  Section 1
+                </Link>
+              </li>
+              <li>
+                <Link to="/form-ref" id="form-ref-link">
+                  Section 2
+                </Link>
+              </li>
+              <li>
+                <Link to="/form-state" id="form-state-link">
+                  Section 3
+                </Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Routes>
+            <Route path="/form" element={<Section1 />} />
+            <Route path="/form-ref" element={<Section2 />} />
+            <Route path="/form-state" element={<Section3 />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  }
+  
+  export default App;
